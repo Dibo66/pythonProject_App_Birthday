ebf1d29f92287c1cca0ad03d09cb396046236199
Iliyan Ivanov <66dibo@gmail.com>
2021-07-31 21:24:05 +0300

    update database.db

--- testAppButtons.py
+++ testAppButtons.py
@@ -2,9 +2,9 @@ from tkinter import *
 
 
 class MyButton(Button):
-    def __init__(self,master,**kwargs):
+    def __init__(self, master, **kwargs):
         super().__init__(master=master, **kwargs)
-        self.my_buttons = {"font":"Sans 12 bold", "width":12}
+        self.my_buttons = {"font": "Sans 12 bold", "width": 12}
         self.config(self.my_buttons)
 
 
